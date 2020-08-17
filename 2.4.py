67207572f68fb6b4c738129f2ced6d5b5cc68bbf
SlavaKrilov <50460016+VlaKriVia@users.noreply.github.com>
2020-08-17 14:00:38 +0300

    Update 2.4.py

--- 2.4.py
+++ 2.4.py
@@ -1,19 +1,5 @@
-my_list = [7, 5, 3, 3, 2]
-
-while True:
-    print(my_list)
-
-    num = int(input())
-    if my_list[0] <= num:
-        my_list.insert(0, num)
-        continue
-
-    count = 0
-    for i in my_list:
-        if my_list[count-1] >= num >= my_list[count]:
-            my_list.insert(count, num)
-            break
-        count += 1
-
-    if num < my_list[-1]:
-        my_list.append(num)
+in_put = input().split()
+count = 1
+for i in in_put:
+    print(f'{count}. {i[0:10:1]}')
+    count += 1
